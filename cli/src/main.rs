d87dbdb6e47bd6de86075e08f5de9ecc0f89be45
Raclett3 <im@s283.net>
2024-07-27 21:35:09 +0900

    CLI を作成

---
+++ cli/src/main.rs
@@ -0,0 +1,51 @@
+use clap::Parser;
+use generator::{
+    bms::chart_to_bms,
+    chord::ChordDensity,
+    generate::{generate_chart, ChartParams},
+};
+use std::{
+    fs::File,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    /// Filename of output BMS
+    filename: String,
+
+    #[arg(long, default_value_t = 150.0)]
+    bpm: f32,
+
+    #[arg(long, default_value_t = 16)]
+    bars: usize,
+
+    #[arg(long)]
+    seed: Option<u64>,
+}
+
+fn seed_from_time() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64
+}
+
+fn main() {
+    let args = Args::parse();
+    let chord_density = ChordDensity::from_power_of_two(&[0, 0, 100, 100, 100]);
+    let seed = args.seed.unwrap_or_else(seed_from_time);
+    let params = ChartParams::new(chord_density, args.bpm, args.bars, seed);
+    let chart = generate_chart(&params);
+
+    let file = File::create(args.filename).expect("Failed to open file");
+    let notes: usize = chart.bars.iter().flatten().map(|chord| chord.len()).sum();
+    let total = f32::max(1000.0 - 1000000.0 / (1000.0 + notes as f32), 250.0);
+
+    if chart_to_bms(&file, &chart, "test", total).is_ok() {
+        println!("BMS の生成に成功しました。");
+    } else {
+        eprintln!("BMS の書き出しに失敗しました。");
+    }
+}
