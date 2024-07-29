da32fdd07af1649613873dcb7b1d3152bc9ec790
Raclett3 <im@s283.net>
2024-07-29 20:17:24 +0900

    基礎的な Web UI を作成

--- web-ui/src/routes/+page.svelte
+++ web-ui/src/routes/+page.svelte
@@ -1,16 +1,123 @@
 <script lang="ts">
-    import { greet } from "$wasm";
+    import { generate_bms } from "$wasm";
 
-    let name = "";
+    let bars = 16;
+    let bpm = 150;
+    let jackTolerance = 0;
+    let chordDensity = [0, 0, 0, 0, 200];
+    let scatter = 0;
+    let scatterDecayRate = 0.5;
+    let seedString = "";
+
+    const chordLabels = ["1分", "2分", "4分", "8分", "16分"];
+
+    function getSeed() {
+        if (seedString === "") {
+            return BigInt(Date.now());
+        } else {
+            return BigInt(seedString);
+        }
+    }
+
+    function downloadURI(uri: string, name: string) {
+        const link = document.createElement("a");
+        link.download = name;
+        link.href = uri;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
+    function dataURI(content: string, mime = "application/octet-stream") {
+        return `data:${mime};base64,${btoa(content)}`;
+    }
 
     function onClick() {
-        greet(name);
+        const chordDensityArray = BigUint64Array.from(chordDensity.map(BigInt));
+        const seed = getSeed();
+        const resultBms = generate_bms(
+            bars,
+            bpm,
+            jackTolerance,
+            chordDensityArray,
+            scatter,
+            scatterDecayRate,
+            seed,
+        );
+
+        if (resultBms === undefined) {
+            alert("BMS の生成に失敗しました。");
+            return;
+        }
+
+        downloadURI(dataURI(resultBms), "result.bms");
     }
 </script>
 
-<h1>Hello, world!</h1>
+<h1>BMS自動生成くん</h1>
 
 <form on:submit|preventDefault={onClick}>
-    <p><input bind:value={name} /></p>
-    <p><button type="submit">Say hello!</button></p>
+    <label>
+        <p>小節数</p>
+        <input type="text" bind:value={bars} />
+    </label>
+    <label>
+        <p>BPM</p>
+        <input type="text" bind:value={bpm} />
+    </label>
+    <div class="density-grid">
+        {#each chordLabels as label}
+            <div>{label}</div>
+        {/each}
+        {#each chordDensity as density}
+            <div><input type="test" bind:value={density} /></div>
+        {/each}
+    </div>
+    <label>
+        <p>縦連許容度</p>
+        <input type="text" bind:value={jackTolerance} />
+    </label>
+    <label>
+        <p>散らばり度</p>
+        <input type="text" bind:value={scatter} />
+        <p><input type="range" min="-10" max="10" bind:value={scatter}></p>
+    </label>
+    <label>
+        <p>シード</p>
+        <input type="text" bind:value={seedString} />
+    </label>
+    <p>
+        <button type="submit">生成</button>
+    </p>
 </form>
+
+<style>
+    label {
+        display: block;
+        margin: 20px 0;
+    }
+
+    label p {
+        margin: 5px 0;
+        color: rgba(0, 0, 0, 0.8);
+    }
+
+    input {
+        padding: 5px;
+    }
+
+    button {
+        box-sizing: border-box;
+    }
+
+    .density-grid {
+        display: grid;
+        grid-template-columns: repeat(5, 100px);
+        gap: 5px;
+    }
+
+    .density-grid input {
+        box-sizing: border-box;
+        width: 100%;
+    }
+</style>
--- web-ui/wasm/src/lib.rs
+++ web-ui/wasm/src/lib.rs
@@ -1,11 +1,54 @@
+use generator::{
+    bms::chart_to_bms,
+    chord::ChordDensity,
+    generate::{generate_chart, ChartParams, Scatter},
+    keysound::{ChordKeySound, ChordRoot, ChordType},
+};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
+static CHORD_PROGRESSION: [(ChordRoot, ChordType); 8] = [
+    (ChordRoot::D, ChordType::Major),
+    (ChordRoot::A, ChordType::Major),
+    (ChordRoot::B, ChordType::Minor),
+    (ChordRoot::Fs, ChordType::Minor),
+    (ChordRoot::G, ChordType::Major),
+    (ChordRoot::D, ChordType::Major),
+    (ChordRoot::G, ChordType::Major),
+    (ChordRoot::A, ChordType::Major),
+];
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn generate_bms(
+    bars: usize,
+    bpm: f32,
+    jack_tolerance: f32,
+    chord_density: &[u64],
+    scatter: f32,
+    scatter_decay_rate: f32,
+    seed: u64,
+) -> Option<String> {
+    let chord_density = ChordDensity::from_power_of_two(chord_density);
+
+    let scatter = Scatter::new(scatter.abs(), scatter_decay_rate, scatter < 0.0);
+
+    let params = ChartParams::new(chord_density, bpm, bars, jack_tolerance, scatter, seed);
+    let chart = generate_chart(&params);
+
+    let notes: usize = chart
+        .bars
+        .iter()
+        .flatten()
+        .map(|chord| chord.lanes.len())
+        .sum();
+    let total = f32::max(1000.0 - 1000000.0 / (1000.0 + notes as f32), 250.0);
+
+    let mut keysounds = ChordKeySound::new(CHORD_PROGRESSION.to_vec());
+
+    let mut bms: Vec<u8> = Vec::new();
+
+    if chart_to_bms(&mut bms, &chart, "test", total, &mut keysounds).is_ok() {
+        unsafe { Some(String::from_utf8_unchecked(bms)) }
+    } else {
+        None
+    }
 }
